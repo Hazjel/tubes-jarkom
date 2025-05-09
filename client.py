01a810cef8cfeeb1adddddef012c65369bc432ce
Hazjel <duojhs222@gmail.com>
2025-05-09 17:22:46 +0700

    first commit

--- client.py
+++ client.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import socket
+import sys
+
+def start_client(ip, port, file):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    client_socket.connect((ip, port))
+
