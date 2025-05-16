b4cc005f82be49d3e1643289d360dbae1e8cec4b
Hazjel <duojhs222@gmail.com>
2025-05-17 06:22:33 +0700

    client has beed updated

--- client.py
+++ client.py
@@ -6,3 +6,26 @@ def start_client(ip, port, file):
     
     client_socket.connect((ip, port))
 
+    request = f"GET /{file} HTTP/1.1\r\nHost: {ip}:{port}\r\n\r\n"
+    
+    client_socket.send(request.encode())
+    response = client_socket.recv(1024).decode()
+    print(response)
+    
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        print(data.decode(), end='')
+        
+    client_socket.close()
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+    
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    file = sys.argv[3]
+    
+    start_client(ip, port, file)
