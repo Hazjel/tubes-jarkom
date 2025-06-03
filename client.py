e19c02ae55173625433f3e850d085cbaf5367d2c
Hazjel <duojhs222@gmail.com>
2025-06-03 21:07:19 +0700

    revisi client

--- client.py
+++ client.py
@@ -6,17 +6,23 @@ def start_client(ip, port, file):
     
     client_socket.connect((ip, port))
 
-    request = f"GET /{file} HTTP/1.1\r\nHost: {ip}:{port}\r\n\r\n"
-    
-    client_socket.send(request.encode())
-    response = client_socket.recv(1024).decode()
-    print(response)
-    
     while True:
-        data = client_socket.recv(1024)
-        if not data:
+        # Ask user if they want to make another request
+        file = input("Enter file name to request (or 'quit' to exit): ")
+        if file.lower() == 'quit':
             break
-        print(data.decode(), end='')
+
+        request = f"GET /{file} HTTP/1.1\r\nHost: {ip}:{port}\r\n\r\n"
+        
+        client_socket.send(request.encode())
+        response = client_socket.recv(1024).decode()
+        print(response)
+        
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print(data.decode(), end='')
     
     client_socket.close()
     
