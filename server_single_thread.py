93c4ed5c64c2f530fa5afd665d6a3fb9ff568470
Revanzavz <revanzavz@student.telkomuniversity.ac.id>
2025-06-03 23:24:29 +0700

    listen

--- server_single_thread.py
+++ server_single_thread.py
@@ -3,7 +3,7 @@ import sys
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('', 5465))
-server_socket.listen(5)
+server_socket.listen(1)
 
 print("Server siap melayani...")
 
